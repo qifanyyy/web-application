96f9be5c3829f2334bd272e5bc65acf6889e95ae
Qifan Yu <qifany4@uci.edu>
2020-04-08 01:21:34 -0700

    front end

---
+++ server/src/MoviesServlet.java
@@ -0,0 +1,114 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+// Declaring a WebServlet called MoviesServlet, which maps to url "/api/movies"
+@WebServlet(name = "MoviesServlet", urlPatterns = "/api/movies")
+public class MoviesServlet extends HttpServlet {
+    private static final long serialVersionUID = 3L;
+
+    // Create a dataSource which registered in web.xml
+    @Resource(name = "jdbc/moviedb")
+    private DataSource dataSource;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        try {
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+
+            // Declare our statement
+            Statement statement = dbcon.createStatement();
+
+            String query = "SELECT movies.*, rating FROM movies, ratings WHERE movies.id = ratings.movieId ORDER BY -rating LIMIT 20;";
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery(query);
+
+            JsonArray jsonArray = new JsonArray();
+
+            // Iterate through each row of rs
+            while (rs.next()) {
+                String movie_id = rs.getString("id");
+                String movie_title = rs.getString("title");
+                String movie_year = rs.getString("year");
+                String movie_director = rs.getString("director");
+                String movie_genre = "";
+                String movie_star = "";
+                String movie_rating = rs.getString("rating");
+
+                query = "SELECT name FROM genres_in_movies, genres WHERE movieId = \""+ movie_id + "\" AND id = genreID;";
+                ResultSet genre_rs = dbcon.createStatement().executeQuery(query);
+                while (genre_rs.next()) {
+                    if (movie_genre != "") {
+                        movie_genre += ", ";
+                    }
+                    movie_genre += genre_rs.getString("name");
+                }
+
+                query = "SELECT name FROM stars_in_movies, stars WHERE movieId = \""+ movie_id + "\" AND id = starID LIMIT 3;";
+                ResultSet star_rs = dbcon.createStatement().executeQuery(query);
+                while (star_rs.next()) {
+                    if (movie_star != "") {
+                        movie_star += ", ";
+                    }
+                    movie_star += star_rs.getString("name");
+                }
+
+
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_id", movie_id);
+                jsonObject.addProperty("movie_title", movie_title);
+                jsonObject.addProperty("movie_year", movie_year);
+                jsonObject.addProperty("movie_director", movie_director);
+                jsonObject.addProperty("movie_genre", movie_genre);
+                jsonObject.addProperty("movie_star", movie_star);
+                jsonObject.addProperty("movie_rating", movie_rating);
+
+                jsonArray.add(jsonObject);
+            }
+            
+            // write JSON string to output
+            out.write(jsonArray.toString());
+            // set response status to 200 (OK)
+            response.setStatus(200);
+
+            rs.close();
+            statement.close();
+            dbcon.close();
+        } catch (Exception e) {
+        	
+			// write error message JSON object to output
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+			out.write(jsonObject.toString());
+
+			// set reponse status to 500 (Internal Server Error)
+			response.setStatus(500);
+        }
+        out.close();
+
+    }
+}
