db23008e8d7073df81a39acf569b91594b75ae6a
Tongjie <tjwang0221@gmail.com>
2020-04-09 13:22:29 -0700

    add functionality to minify html, css and js

---
+++ server/minify.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+import sys
+
+html_minify_cmd = 'html-minifier --collapse-whitespace --remove-comments --remove-optional-tags ' + \
+    '--remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype ' + \
+    '{} -o {}'
+clean_css_cmd = 'cleancss -O2 {} -o {}'
+uglify_js_cmd = 'uglifyjs {} -o {} --compress --mangle --drop_console'
+
+
+def get_output_file_name(input_file: Path) -> Path:
+    if len(sys.argv) < 2 or sys.argv[1] != '--deploy':
+        return input_file.parent / (input_file.stem + '.min' + input_file.suffix)
+    return input_file
+
+
+if __name__ == '__main__':
+    for f in filter(
+            lambda x: x.suffix in {'.html', '.css', '.js'} and '.min' not in set(x.suffixes),
+            sorted(Path('./WebContent').iterdir())
+    ):
+        print('minifying {}'.format(f.name))
+        f = f.absolute()
+        if f.suffix == '.html':
+            if ret := os.system(html_minify_cmd.format(f, f.parent / (f.stem + '.min' + f.suffix))) != 0:
+                exit(ret)
+        elif f.suffix == '.css':
+            if ret := os.system(clean_css_cmd.format(f, f.parent / (f.stem + '.min' + f.suffix))) != 0:
+                exit(ret)
+        else:  # .js
+            if ret := os.system(uglify_js_cmd.format(f, f.parent / (f.stem + '.min' + f.suffix))) != 0:
+                exit(ret)
