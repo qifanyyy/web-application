a66df843d47cd5a0311a09e176aa3f0f6bc7e303
Qifan Yu <qifany4@uci.edu>
2020-05-11 11:40:50 -0700

    Update stored-procedure.sql

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -6,8 +6,7 @@ CREATE PROCEDURE add_movie(
 	arg_year INTEGER,
 	arg_director VARCHAR(100),
 	arg_genre_name VARCHAR(32),
-	arg_star_first_name VARCHAR(50),
-	arg_star_last_name VARCHAR(50),
+	arg_star_name VARCHAR(100),
 	arg_star_dob DATE)
 BEGIN
 	SET movie_count = (SELECT count(*) FROM movies AS m WHERE 
@@ -17,13 +16,11 @@ BEGIN
 	SET genre_count = (SELECT count(*) FROM genres AS g WHERE g.name = arg_genre_name);
 	IF arg_star_dob IS NULL THEN
 		SET star_count = (SELECT count(*) FROM stars AS s WHERE
-			s.first_name = arg_star_first_name AND
-			s.last_name = arg_star_last_name AND 
+			s.name = arg_star_name AND
 			s.dob IS NULL);
 	ELSE
 		SET star_count = (SELECT count(*) FROM stars AS s WHERE
-			s.first_name = arg_star_first_name AND
-			s.last_name = arg_star_last_name AND 
+			s.name = arg_star_first_name AND
 			s.dob = arg_star_dob);
 	END IF;
 	IF movie_count = 0 THEN
@@ -40,17 +37,15 @@ BEGIN
 	SET tmp_genre_id = (SELECT max(id) FROM genres AS g WHERE g.name = arg_genre_name);
 	IF star_count = 0 THEN
 		INSERT INTO stars (first_name, last_name, dob) VALUES 
-			(arg_star_first_name, arg_star_last_name, arg_star_dob);
+			(arg_star_name, arg_star_dob);
 	END IF;
 	IF arg_star_dob IS NULL THEN
 		SET tmp_star_id = (SELECT max(id) FROM stars AS s WHERE
-			s.first_name = arg_star_first_name AND
-			s.last_name = arg_star_last_name AND 
+			s.name = arg_star_name AND
 			s.dob IS NULL);
 	ELSE
 		SET tmp_star_id = (SELECT max(id) FROM stars AS s WHERE
-			s.first_name = arg_star_first_name AND
-			s.last_name = arg_star_last_name AND 
+			s.name = arg_star_name AND
 			s.dob = arg_star_dob);
 	END IF;
 	SET genres_in_movies_count = (SELECT count(*) from genres_in_movies AS gim WHERE gim.genre_id = tmp_genre_id AND gim.movie_id = tmp_movie_id);
