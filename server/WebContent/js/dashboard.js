b3c8f28ea77e56d7ed4755aa287398b9f14cde50
Tongjie <tjwang0221@gmail.com>
2020-05-10 21:35:03 -0700

    simple dashboard forms

--- server/WebContent/js/dashboard.js
+++ server/WebContent/js/dashboard.js
@@ -20,6 +20,30 @@ function handleResult(json) {
     showcaseRootDiv.innerHTML += '</ul>'
 }
 
+function onSubmit(event, form) {
+    event.preventDefault()
+    fetch('api/dashboard', {
+        method: 'POST',
+        body: new URLSearchParams(new FormData(form))
+    }).then(response => response.json(), reason => {
+        console.error(reason)
+        alert(`Failed to add`)
+    }).then(json => {
+        if (json['status'] !== 'success') {
+            jsonErrorMsgHandler(json)
+            alert(`Failed to add`)
+        } else {
+            alert(`Success!`)
+        }
+    })
+}
+
+const addStarForm = document.getElementById('add-star-form')
+addStarForm.addEventListener('submit', (ev) => onSubmit(ev, addStarForm))
+
+const addMovieForm = document.getElementById('add-movie-form')
+addMovieForm.addEventListener('submit', ev => onSubmit(ev, addMovieForm))
+
 fetch('api/dashboard')
     .then(response => response.json(), reason => console.error(reason))
     .then(json => handleResult(json))
