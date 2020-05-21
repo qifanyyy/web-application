34f6ce9c6ed8a9f6997c14c7f8aaeb457e373128
Qifan Yu <qifany4@uci.edu>
2020-05-21 05:26:47 -0700

    Autocomplete API

--- server/src/Suggestion.java
+++ server/src/Suggestion.java
@@ -1,11 +1,16 @@
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.HashMap;
 
+import javax.annotation.Resource;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -14,78 +19,14 @@ import com.google.gson.JsonObject;
 @WebServlet("/suggestion")
 public class Suggestion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	/*
-	 * populate the Super hero hash map.
-	 * Key is hero ID. Value is hero name.
-	 */
-	public static HashMap<Integer, String> superHeroMap = new HashMap<>();
-	
-	static {
-		superHeroMap.put(1, "Blade");
-		superHeroMap.put(2, "Ghost Rider");
-		superHeroMap.put(3, "Luke Cage");
-		superHeroMap.put(4, "Silver Surfer");
-		superHeroMap.put(5, "Beast");
-		superHeroMap.put(6, "Thing");
-		superHeroMap.put(7, "Black Panther");
-		superHeroMap.put(8, "Invisible Woman");
-		superHeroMap.put(9, "Nick Fury");
-		superHeroMap.put(10, "Storm");
-		superHeroMap.put(11, "Iron Man");
-		superHeroMap.put(12, "Professor X");
-		superHeroMap.put(13, "Hulk");
-		superHeroMap.put(14, "Cyclops");
-		superHeroMap.put(15, "Thor");
-		superHeroMap.put(16, "Jean Grey");
-		superHeroMap.put(17, "Wolverine");
-		superHeroMap.put(18, "Daredevil");
-		superHeroMap.put(19, "Captain America");
-		superHeroMap.put(20, "Spider-Man");
 
-		superHeroMap.put(101, "Superman");
-		superHeroMap.put(102, "Batman");
-		superHeroMap.put(103, "Wonder Woman");
-		superHeroMap.put(104, "Flash");
-		superHeroMap.put(105, "Green Lantern");
-		superHeroMap.put(106, "Catwoman");
-		superHeroMap.put(107, "Nightwing");
-		superHeroMap.put(108, "Captain Marvel");
-		superHeroMap.put(109, "Aquaman");
-		superHeroMap.put(110, "Green Arrow");
-		superHeroMap.put(111, "Martian Manhunter");
-		superHeroMap.put(112, "Batgirl");
-		superHeroMap.put(113, "Supergirl");
-		superHeroMap.put(114, "Black Canary");
-		superHeroMap.put(115, "Hawkgirl");
-		superHeroMap.put(116, "Cyborg");
-		superHeroMap.put(117, "Robin");
-	}
+	@Resource(name = "jdbc/moviedb")
+	private DataSource dataSource;
     
     public Suggestion() {
         super();
     }
 
-    /*
-     * 
-     * Match the query against superheroes and return a JSON response.
-     * 
-     * For example, if the query is "super":
-     * The JSON response look like this:
-     * [
-     * 	{ "value": "Superman", "data": { "heroID": 101 } },
-     * 	{ "value": "Supergirl", "data": { "heroID": 113 } }
-     * ]
-     * 
-     * The format is like this because it can be directly used by the 
-     *   JSON auto complete library this example is using. So that you don't have to convert the format.
-     *   
-     * The response contains a list of suggestions.
-     * In each suggestion object, the "value" is the item string shown in the dropdown list,
-     *   the "data" object can contain any additional information.
-     * 
-     * 
-     */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		try {
 			// setup the response json arrray
@@ -100,16 +41,39 @@ public class Suggestion extends HttpServlet {
 				return;
 			}	
 			
-			// search on superheroes and add the results to JSON Array
-			// this example only does a substring match
+
 			// TODO: in project 4, you should do full text search with MySQL to find the matches on movies and stars
-			
-			for (Integer id : superHeroMap.keySet()) {
-				String heroName = superHeroMap.get(id);
-				if (heroName.toLowerCase().contains(query.toLowerCase())) {
-					jsonArray.add(generateJsonObject(id, heroName));
-				}
+
+			String fulltext = "";
+			String[] fulltextarray = query.split(" ");
+			for (int i = 0; i < fulltextarray.length; i++) fulltext += "+" + fulltextarray[i] + "* ";
+
+			System.out.println("query:" + fulltext);
+			PreparedStatement getTitle = null;
+			String titleQuery = "SELECT * FROM movies WHERE match(title) against (? IN BOOLEAN MODE) LIMIT 10; ";
+
+
+
+			Connection con = dataSource.getConnection();
+
+			getTitle = con.prepareStatement(titleQuery);
+			getTitle.setString(1, query);
+			ResultSet rs = getTitle.executeQuery();
+
+			if (!rs.next()) {
+				response.getWriter().write(jsonArray.toString());
+				con.close();
+				return;
 			}
+
+
+			while (rs.next()){
+				jsonArray.add(generateJsonObject(rs.getString("id"), rs.getString("title")));
+			}
+
+			rs.close();
+			con.close();
+
 			
 			response.getWriter().write(jsonArray.toString());
 			return;
@@ -127,12 +91,12 @@ public class Suggestion extends HttpServlet {
 	 * }
 	 * 
 	 */
-	private static JsonObject generateJsonObject(Integer heroID, String heroName) {
+	private static JsonObject generateJsonObject(String movieID, String title) {
 		JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("value", heroName);
+		jsonObject.addProperty("value", title);
 		
 		JsonObject additionalDataJsonObject = new JsonObject();
-		additionalDataJsonObject.addProperty("heroID", heroID);
+		additionalDataJsonObject.addProperty("heroID", movieID);
 		
 		jsonObject.add("data", additionalDataJsonObject);
 		return jsonObject;
