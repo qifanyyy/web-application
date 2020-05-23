689411e852df92ec231b119cfce55620fffa70ed
Tongjie <tjwang0221@gmail.com>
2020-05-23 05:09:24 -0700

    remove main.c from fuzzy_search

--- fuzzy_search/min_edit_distance.h
+++ fuzzy_search/min_edit_distance.h
@@ -6,8 +6,4 @@
 int min_edit_distance_init(UDF_INIT* init_id, UDF_ARGS* args, char* message);
 long long min_edit_distance(UDF_INIT* init_id, UDF_ARGS* args, char* is_null, char* error);
 
-#ifdef DEBUG
-int min_edit_distance_dp(const char *s1, const char *s2, size_t s1_len, size_t s2_len);
-#endif
-
 #endif  // FUZZY_SEARCH_MIN_EDIT_DISTANCE_H
