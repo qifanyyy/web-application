1e0af38c48134df617fdecc57f2bf72213d9899d
Tongjie <tjwang0221@gmail.com>
2020-05-23 02:29:01 -0700

    edit distance DP implementation

---
+++ fuzzy_search/README.md
@@ -0,0 +1,38 @@
+# fuzzy_search
+
+## Build
+
+You may use CLion to view, edit, build and run this C++ project.
+
+To build it from command line, make sure you have a modern C++
+compiler (we use Clang 10) and CMake (3.16 or newer) installed.
+
+Then, make sure that your current directory contains this `README.md`
+file.
+
+Create a build directory named `cmake-build-debug` in side current
+working directory.
+
+```shell script
+mkdir cmake-build-debug
+```
+
+Go to `cmake-build-debug` and start CMake.
+
+```shell script
+cd cmake-build-debug
+cmake ..
+```
+
+Finally, build the library.
+
+```shell script
+make
+```
+
+You can find `main` and `libfuzzy_search.so` (`libfuzzy_search.dylib`
+on macOS) inside `cmake-build-debug` if build process succeeded.
+
+## Clean
+
+Just remove the `cmake-build-debug` directory.
--- fuzzy_search/edit_distance.cpp
+++ fuzzy_search/edit_distance.cpp
@@ -1,5 +1,27 @@
 #include "edit_distance.hpp"
 
-int editDistance() {
-    return -3;
+#include <string>
+#include <vector>
+
+int editDistanceDP(const std::string& s1, const std::string& s2) {
+    std::vector<std::vector<int>> dp(s1.length() + 1, std::vector<int>(s2.length() + 1, 0));
+    for (int i = 0; i < s1.length() + 1; ++i) {
+        dp.at(i).front() = i;
+    }
+    for (int j = 0; j < s2.length() + 1; ++j) {
+        dp.front().at(j) = j;
+    }
+    for (int i = 1; i < s1.length() + 1; ++i) {
+        for (int j = 1; j < s2.length() + 1; ++j) {
+            if (s1.at(i - 1) == s2.at(j - 1))
+                dp.at(i).at(j) = dp.at(i - 1).at(j - 1);
+            else
+                dp.at(i).at(j) = 1 + std::min({dp.at(i - 1).at(j), dp.at(i).at(j - 1), dp.at(i - 1).at(j - 1)});
+        }
+    }
+    return dp.back().back();
+}
+
+long long editDistance(const char* s1, const char* s2) {
+    return editDistanceDP(std::string(s1), std::string(s2));
 }
