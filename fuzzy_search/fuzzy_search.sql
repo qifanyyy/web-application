71f2dbc3aea23d15218730051a2c45135f3b3b16
Tongjie <tjwang0221@gmail.com>
2020-05-24 23:31:43 -0700

    update fuzzy_search; add tests

--- fuzzy_search/fuzzy_search.sql
+++ fuzzy_search/fuzzy_search.sql
@@ -1,2 +1,2 @@
 DROP FUNCTION IF EXISTS min_edit_distance;
-CREATE FUNCTION min_edit_distance RETURNS INTEGER SONAME 'lib_fuzzy_search.so';
+CREATE FUNCTION min_edit_distance RETURNS INTEGER SONAME 'libfuzzy_search.so';
