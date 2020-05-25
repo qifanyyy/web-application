7219da1e4446dd49f82e9284da92bb0aaccad08b
Tongjie <tjwang0221@gmail.com>
2020-05-24 17:19:11 -0700

    small tweak to login ui to make demo easier

--- android/app/src/main/java/fabflixmobile/Login.java
+++ android/app/src/main/java/fabflixmobile/Login.java
@@ -1,6 +1,7 @@
 package fabflixmobile;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -24,7 +26,6 @@ public class Login extends ActionBarActivity {
     private EditText username;
     private EditText password;
     private TextView message;
-    private Button loginButton;
     private String url;
 
     @Override
@@ -35,63 +36,48 @@ public class Login extends ActionBarActivity {
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         message = findViewById(R.id.message);
-        loginButton = findViewById(R.id.login);
+        Button loginButton = findViewById(R.id.login);
         /**
          * In Android, localhost is the address of the device or the emulator.
          * To connect to your machine, you need to use the below IP address
          * **/
-        url = "https://10.0.2.2:8443/server_war/api/";
+        url = "https://10.0.2.2:8443/api/";
         
 
 
         //assign a listener to call a function to handle the user request when clicking a button
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login();
-            }
-        });
+        loginButton.setOnClickListener(view -> login());
     }
 
     public void login() {
-
-        message.setText("Trying to login");
+        message.setText("Logging in...");
         // Use the same network queue across our application
         final RequestQueue queue = NetworkManager.sharedManager(this).queue;
         //request type is POST
-        final StringRequest loginRequest = new StringRequest(Request.Method.POST, url + "login", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                //TODO should parse the json response to redirect to appropriate functions.
-
-
-                try {
-                    JSONObject responseJson = new JSONObject(response);
-                    Log.d("responseJson", responseJson.getString("status"));
-                    if (responseJson.getString("status").equals("success")) {
-                        Log.d("login.success", response);
-                        //initialize the activity(page)/destination
-                        Intent listPage = new Intent(Login.this, ListViewActivity.class);
-                        //without starting the activity/page, nothing would happen
-                        startActivity(listPage);
-                    }
-                    else {
-                        message.setText("invalid email or password");
-                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
+        final StringRequest loginRequest = new StringRequest(Request.Method.POST, url + "login", response -> {
+            try {
+                JSONObject responseJson = new JSONObject(response);
+                Log.d("response", response);
+                Log.d("responseJson", responseJson.getString("status"));
+                if (responseJson.getString("status").equals("success")) {
+                    Log.d("login.success", response);
+                    //initialize the activity(page)/destination
+                    Intent listPage = new Intent(Login.this, ListViewActivity.class);
+                    //without starting the activity/page, nothing would happen
+                    startActivity(listPage);
+                }
+                else {
+                    message.setText(responseJson.getString("message"));
                 }
 
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // error
-                        Log.d("login.error", error.toString());
-                    }
-                }) {
+
+        }, error -> {
+            // error
+            Log.e("login.error", error.toString());
+        }) {
             @Override
             protected Map<String, String> getParams() {
                 // Post request form data
@@ -105,6 +91,5 @@ public class Login extends ActionBarActivity {
 
         // !important: queue.add is where the login request is actually sent
         queue.add(loginRequest);
-
     }
 }
--- server/src/Util.java
+++ server/src/Util.java
@@ -9,6 +9,7 @@ public class Util {
     public static JsonObject exception2Json(Exception e) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("status", "failed");
+        jsonObject.addProperty("message", e.getMessage());
         jsonObject.addProperty("errorMessage", e.getMessage());
         StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
@@ -20,6 +21,7 @@ public class Util {
     public static JsonObject makeGeneralErrorJsonObject(String errorMessage) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("status", "failed");
+        jsonObject.addProperty("message", errorMessage);
         jsonObject.addProperty("errorMessage", errorMessage);
         return jsonObject;
     }
